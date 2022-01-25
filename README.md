637deff5bc274da92ac931911951a7ed9e0ad753
hashem98 <hashemsmadi98@gmail.com>
2022-01-25 21:00:20 +0200

    Day8

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HR-management-system
+
+* I added a employee form
+* I Created a function to generate a unique four digits employee id number
+* I added an event listener to get the data from the form instead of having hard coded data.
+* I refactored the render prototype function to render each employee information in a separate card 
+# Please stick to the names of the employees in the assest file
--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 'use strict';
-
+let empForm = document.getElementById('empForm');
+let empSec = document.getElementById('empSec');
+empSec.style.display="inline";
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-function Employee(employeeId, fullName, department, level, imageUrl , salary) {
+function Employee(employeeId, firstName ,lastName, department, level ,salary,imageUrl ) {
   this.employeeId = employeeId;
-  this.fullName = fullName;
+  this.firstName=firstName;
+  this.lastName=lastName;
+  this.fullName = firstName+" "+lastName;
+  // let firstNameCap=this.firstName[0].toUpperCase()+this.firstName.substring(1);
   this.department = department;
   this.level = level;
-  this.imageUrl = imageUrl;
-this.salary=0;
+  this.imageUrl = `../assets/${this.firstName}.jpg` ;
+  this.salary=salary;
 
 }
 Employee.prototype.getSalary = function () {
@@ -33,28 +38,105 @@ Employee.prototype.getSalary = function () {
   
 
   netSalary = balance - tax;
+  this.salary=netSalary;
+  
   return netSalary;
 }
-const ghazi = new Employee(1000, "Ghazi Samer", "Administration", "Senior", "");
-const lana = new Employee(1001, "Lana Ali", "Finance", "Senior", "");
-const tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "");
-const safi = new Employee(1003, "Safi Walid", "Administration", "Senior", "");
-const omar = new Employee(1004, "Omar Zaid", "	Development", "Senior", "");
-const rana = new Employee(1005, "Rana Saleh", "Development", "Senior", "");
-const hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Senior", "");
+Employee.prototype.render = function () {
+  let div1 = document.createElement('div');
+let img = document.createElement('img');
+let div2 = document.createElement('div');
+let h5 = document.createElement('h5');
+// let p = document.createElement('p');
+
+       div1.setAttribute("class","card")
+       img.setAttribute("class","img")
+       div1.setAttribute
+      div1.appendChild(img);
+      h5.textContent=` Name: ${this.fullName}  ID:${this.employeeId}  Department:${this.department} 
+                        Level:${this.level}
+                        Salary:${this.salary}
+      `;
+      div2.setAttribute("class","container");
+      div2.appendChild(h5);
+      div1.appendChild(div2);
+      
+     
+
+      img.setAttribute('src',this.imageUrl);
+     empSec.appendChild(div1);
+     
+
+}
+var generatID = (function(num) {
+  return function() {
+    var str = String(num++);
+    while (str.length < 4) str = "0" + str;
+    return str;
+  }
+})(1);
+function handelSubmit(event){
+  event.preventDefault();
+  let firstName = event.target.fn.value ;
+  let lastName = event.target.ln.value ;
+  let department=event.target.departments.value;
+  let level=event.target.Level.value;
+  let imag=event.target.img.value;
+   let newemp = new Employee(generatID(),firstName[0].toUpperCase()+firstName.substring(1),lastName,department, level);
+  let salary1=newemp.getSalary();
+  newemp.render();
+
+}
+empForm.addEventListener('submit', handelSubmit);
+
+
+      // let firstNameCap=this.firstName[0].toUpperCase()+this.firstName.substring(1);
+
+    // const ghazi = new Employee(1000, "Ghazi","asad", "Administration", "Senior");
+    // ghazi.render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ghazi = new Employee(1000, "Ghazi Samer", "Administration", "Senior", "");
+// const lana = new Employee(1001, "Lana Ali", "Finance", "Senior", "");
+// const tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "");
+// const safi = new Employee(1003, "Safi Walid", "Administration", "Senior", "");
+// const omar = new Employee(1004, "Omar Zaid", "	Development", "Senior", "");
+// const rana = new Employee(1005, "Rana Saleh", "Development", "Senior", "");
+// const hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Senior", "");
 
 // Employee.prototype.render = function () {
 //   document.write(`<p> ${this.fullName} : ${this.getSalary()} </p>`)
 // }
 // ghazi.render();
-document.write("<br><br><br><br><br>")
-document.write(` <p> ${ghazi.fullName} : ${ghazi.getSalary()}</p>`);
-document.write(`<p>${lana.fullName} : ${lana.getSalary()}</p>`);
-document.write(`<p>${tamara.fullName} : ${tamara.getSalary()}</p>`);
-document.write(`<p>${safi.fullName} : ${safi.getSalary()}</p>`);
-document.write(`<p>${omar.fullName} : ${omar.getSalary()}</p>`);
-document.write(`<p>${rana.fullName} : ${rana.getSalary()}</p>`);
-document.write(`<p>${hadi.fullName} : ${hadi.getSalary()}</p>`);
+// document.write("<br><br><br><br><br>")
+// document.write(` <p> ${ghazi.fullName} : ${ghazi.getSalary()}</p>`);
+// document.write(`<p>${lana.fullName} : ${lana.getSalary()}</p>`);
+// document.write(`<p>${tamara.fullName} : ${tamara.getSalary()}</p>`);
+// document.write(`<p>${safi.fullName} : ${safi.getSalary()}</p>`);
+// document.write(`<p>${omar.fullName} : ${omar.getSalary()}</p>`);
+// document.write(`<p>${rana.fullName} : ${rana.getSalary()}</p>`);
+// document.write(`<p>${hadi.fullName} : ${hadi.getSalary()}</p>`);
 
 
 
